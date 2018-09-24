ef2674736b0ecb33ff7c4cda8f004e2685b9940a
Julian Gründner <juliangruendner@googlemail.com>
2018-09-24 12:57:31 +0200

    improve installation messages

--- install_prod.sh
+++ install_prod.sh
@@ -2,22 +2,21 @@
 export ADMIN=${ADMIN:='administrator'}
 export OPAL_ADMIN_PASS=${OPAL_ADMIN_PASS:='password'}
 
-
-echo "Initialising Opal ..."
+printf "######################\nInitialising Opal ...\n######################\n\n"
 ./start_prod.sh
 echo "waiting 60 second for opal to start ..."
 sleep 60
 ./stop_prod.sh
 
-echo "Configruing opal r server"
+printf "Configuring opal r server \n"
 sed -i 's/#org.obiba.opal.Rserve.host=/org.obiba.opal.Rserve.host=datashield_rserver/g' ds_data/opal/conf/opal-config.properties
 
 ./start_prod.sh
 
-echo "sleep for a bit (1 minute) to wait for opal to start up ..."
+printf "sleep for a bit (1 minute) to wait for opal to start up ...\n"
 sleep 60
 
-echo "Begin Configure Test data an Project ..."
+printf "Begin Configure Test data an Project ...\n"
 # start test docker to input data into opal via python client
 
 cd ../ds_test && docker-compose up -d
@@ -25,7 +24,7 @@ cd ../datashield_docker
 
 
 
-echo "done sleeping, time to configure opal ..."
+printf "done sleeping, time to configure opal ...\n"
 
 # set db
 echo '{"name": "_identifiers", "defaultStorage":false, "usage": "STORAGE", "usedForIdentifiers":true, "mongoDbSettings" : {"url":"mongodb://datashield_mongo:27017/opal_ids"}}' | docker exec --interactive ds_test sh -c "opal rest --opal https://datashield_opal:8443 -u $ADMIN -p $OPAL_ADMIN_PASS --content-type 'application/json' -m POST /system/databases" 
@@ -50,9 +49,12 @@ docker exec ds_test sh -c "opal perm-table --opal https://datashield_opal:8443 -
 # give test user permission to use datashield
 docker exec ds_test sh -c "opal perm-datashield --opal https://datashield_opal:8443 --user $ADMIN --password $OPAL_ADMIN_PASS --type USER --subject test --permission use --add"
 
-echo "Successfully Configured Test data an Project"
+cd ../ds_test && docker-compose stop
+cd ../datashield_docker
 
-echo "Installing datashield R packages on R server ..."
+printf "Successfully Configured Test data an Project \n"
+
+printf "Installing datashield R packages on R server ...\n"
 
 # install datashield packages
 docker exec datashield_rserver R -e "install.packages('datashield', repos=c('https://cran.rstudio.com','https://cran.obiba.org'), dependencies=TRUE)"
@@ -60,9 +62,9 @@ docker exec datashield_rserver R -e "install.packages('datashield', repos=c('htt
 ##initialise datashield configuraton in opal
 python3 insert_base_ds_config.py
 
-echo "Finished installing R packages on R server ..."
+printf "Finished installing R packages on R server ...\n"
 
-echo "Restarting opal to update opal configurations ..."
+printf "Restarting opal to update opal configurations ...\n"
 
 ./stop_prod.sh
 ./start_prod.sh
@@ -72,4 +74,4 @@ echo "finished setting up Opal  - installing R on VM now to allow tests to run"
 # finally install R on vm for test purposes
 # ./install_r.sh
 
-printf "R intsalled and opal set up - you are ready to go \n"
+printf "######################\n R intsalled and opal set up - you are ready to go \n######################\n\n"
